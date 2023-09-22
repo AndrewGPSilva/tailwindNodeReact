2439033ab3095859c244453e9e8203db9ebf3af2
Andrew Gomes <andrewgomes1328@gmail.com>
2023-09-22 20:37:56 -0300

    update

--- src/components/ComponentsWatch/Movies.js
+++ src/components/ComponentsWatch/Movies.js
@@ -1,31 +1,38 @@
 import React from "react";
 import dados from "../../data.json";
+import Card from "../ComponentsWatch/Card.js"
 
 export default function Movies() {
   return (
-    <div className="flex flex-col py-4 px-4 text-black">
+    <div className="flex flex-col py-4 px-4 text-white  bg-black">
       <div className="text-center">
-        <h1 className="text-4xl underline">To Watch</h1>
+        <h1 className="text-4xl">To Watch</h1>
       </div>
       <div className="py-5">
-        <h2 className="underline">Movies:</h2>
-        <ul>
-          {dados.filmes.map((filme) => (
-            <li key={filme.id}>
-              Name: {filme.nome} | Release Year: {filme.ano_de_lancamento}
-            </li>
+        <h2 className="underline font-bold">Movies:</h2>
+        <div>
+          {dados.filmes.map((movie) => (
+            <Card
+              key={movie.id}
+              imagem={movie.imagem_capa}
+              nome={movie.nome}
+              ano={movie.ano_de_lancamento}
+            />
           ))}
-        </ul>
+        </div>
       </div>
       <div className="py-5">
-        <h2 className="underline">Series:</h2>
-        <ul>
+        <h2 className="underline font-bold">Series:</h2>
+        <div>
           {dados.series.map((serie) => (
-            <li key={serie.id}>
-              Name: {serie.nome} | Release Year: {serie.ano_de_lancamento}
-            </li>
+            <Card
+              key={serie.id}
+              imagem={serie.imagem_capa}
+              nome={serie.nome}
+              ano={serie.ano_de_lancamento}
+            />
           ))}
-        </ul>
+        </div>
       </div>
     </div>
   );
